374ade5f98b01c56ad940e09b2779196733af1c8
Jonathan Cheng <jcheng461@gmail.com>
2022-05-17 22:25:29 +0900

    pass 4th test -- determine if three sides makes an isosceles triangle

--- src/triangle.js
+++ src/triangle.js
@@ -4,6 +4,7 @@ export default function Triangle(side1, side2, side3) {
   this.side3 = side3;
 }
 
+// Method to check what type of triangle it is.
 Triangle.prototype.checkType = function () {
   if (
     this.side1 > this.side2 + this.side3 ||
